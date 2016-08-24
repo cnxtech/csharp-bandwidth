bbc99e1b8b40952302ce2fe7d8bed6f1c8bc72d7
Unknown <belchikovs@gmail.com>
2016-08-24 10:32:19 +0300

    Added CallbackEvent

--- src/Bandwidth.Net/JsonStringEnumConverter.cs
+++ src/Bandwidth.Net/JsonStringEnumConverter.cs
@@ -10,10 +10,10 @@ namespace Bandwidth.Net
   {
     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
     {
-      // convert string like "enum-value" to EnumType.EnumValue
+      // convert string like "enum-value" and "eNUM-VALUE" to EnumType.EnumValue
       var rawString = (string)reader.Value;
       var result = string.Join("",
-        rawString.Split('-').Select(v => $"{char.ToUpperInvariant(v[0])}{v.Substring(1)}"));
+        rawString.Split('-').Select(v => $"{char.ToUpperInvariant(v[0])}{v.Substring(1).ToLowerInvariant()}"));
       return Enum.Parse(objectType, result);
     }
 
