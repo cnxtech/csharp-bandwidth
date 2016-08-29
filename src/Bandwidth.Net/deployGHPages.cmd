de5aa3478eb41ccad01af4a9d503911a58659bc3
Unknown <belchikovs@gmail.com>
2016-08-29 15:40:46 +0300

    Added work with github auth token

--- src/Bandwidth.Net/deployGHPages.cmd
+++ src/Bandwidth.Net/deployGHPages.cmd
@@ -12,14 +12,22 @@ rd /s /q Help Pages
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Bandwidth.Net.Html.shfbproj
 git clone https://github.com/bandwidthcom/csharp-bandwidth.git Pages
 cd Pages
+git config user.name "AppVeyor CI"
+git config user.email "%APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL%"
 git checkout gh-pages || git checkout -b gh-pages
 git rm -rf .
 xcopy ..\Help\* . /s /q
 git add .
-set COMMIT_MESSAGE=Updated API docs
+set COMMIT_MESSAGE=Updated API docs: %APPVEYOR_REPO_COMMIT%
 if  defined APPVEYOR_PULL_REQUEST_TITLE (
     set COMMIT_MESSAGE=%COMMIT_MESSAGE%: %APPVEYOR_PULL_REQUEST_TITLE%
 )
 git commit -m "%COMMIT_MESSAGE%"
+(
+    echo machine github.com
+    echo login %APPVEYOR_ACCOUNT_NAME%
+    echo password %GITHUB_TOKEN%
+) > %HOMEDRIVE%%HOMEPATH%\.netrc
+copy %HOMEDRIVE%%HOMEPATH%\.netrc %HOMEDRIVE%%HOMEPATH%\_netrc
 git push origin gh-pages
 cd ..
