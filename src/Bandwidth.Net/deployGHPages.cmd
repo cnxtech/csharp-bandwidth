dc4e11e6b39333bb7e7b6fee3bb724f77e15ff80
Unknown <belchikovs@gmail.com>
2016-08-29 13:42:23 +0300

    Fixed found issues

--- src/Bandwidth.Net/deployGHPages.cmd
+++ src/Bandwidth.Net/deployGHPages.cmd
@@ -1,5 +1,6 @@
+set
 @echo off
-if "%APPVEYOR_REPO_BRANCH%" == "v3-preview" goto cont
+if %APPVEYOR_REPO_BRANCH% == v3-preview goto cont
 goto exit
 :cont
 rd /s /q Help Pages
@@ -10,9 +11,9 @@ git checkout gh-pages || git checkout -b gh-pages
 git rm -rf .
 xcopy ..\Help\* . /s /q
 git add .
-set COMMIT_MESSAGE = "Updated API docs"
-if  "%APPVEYOR_PULL_REQUEST_TITLE%" == "" goto next
-COMMIT_MESSAGE="%COMMIT_MESSAGE%: %APPVEYOR_PULL_REQUEST_TITLE%"
+set COMMIT_MESSAGE=Updated API docs
+if  %APPVEYOR_PULL_REQUEST_TITLE% == "" goto next
+set COMMIT_MESSAGE=%COMMIT_MESSAGE%: %APPVEYOR_PULL_REQUEST_TITLE%
 :next
 git commit -m "%COMMIT_MESSAGE%"
 git push origin gh-pages
