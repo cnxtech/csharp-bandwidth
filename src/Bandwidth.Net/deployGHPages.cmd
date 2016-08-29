7f5a4e68428a029f3a6820989b31b7f660010072
Unknown <belchikovs@gmail.com>
2016-08-29 14:40:00 +0300

    Fixed issues in  deploy script

--- src/Bandwidth.Net/deployGHPages.cmd
+++ src/Bandwidth.Net/deployGHPages.cmd
@@ -1,8 +1,13 @@
-set
 @echo off
-if %APPVEYOR_REPO_BRANCH% == v3-preview goto cont
-goto exit
-:cont
+if not defined BRANCH (
+    set BRANCH=master
+)
+if not defined APPVEYOR_REPO_BRANCH (
+    exit 1
+)  
+if not %APPVEYOR_REPO_BRANCH% == %BRANCH% (
+    exit 1
+)
 rd /s /q Help Pages
 %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Bandwidth.Net.Html.shfbproj
 git clone https://github.com/bandwidthcom/csharp-bandwidth.git Pages
@@ -12,10 +17,9 @@ git rm -rf .
 xcopy ..\Help\* . /s /q
 git add .
 set COMMIT_MESSAGE=Updated API docs
-if  %APPVEYOR_PULL_REQUEST_TITLE% == "" goto next
-set COMMIT_MESSAGE=%COMMIT_MESSAGE%: %APPVEYOR_PULL_REQUEST_TITLE%
-:next
+if  defined APPVEYOR_PULL_REQUEST_TITLE (
+    set COMMIT_MESSAGE=%COMMIT_MESSAGE%: %APPVEYOR_PULL_REQUEST_TITLE%
+)
 git commit -m "%COMMIT_MESSAGE%"
 git push origin gh-pages
 cd ..
-:exit
