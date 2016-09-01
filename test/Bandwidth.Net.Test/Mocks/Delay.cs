cff74375f5b08cf8b8e28f3c721c42eb6349c51e
Unknown <belchikovs@gmail.com>
2016-09-01 12:04:29 +0300

    Added mock classes for tests

---
+++ test/Bandwidth.Net.Test/Mocks/Delay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bandwidth.Net.Api;
+using LightMock;
+
+namespace Bandwidth.Net.Test.Mocks
+{
+    internal class Delay: IDelay
+    {
+      private readonly IInvocationContext<IDelay> _context;
+
+      public Delay(IInvocationContext<IDelay> context)
+      {
+        _context = context;
+      }
+
+      Task IDelay.Delay(TimeSpan interval, CancellationToken cancellationToken)
+      {
+        return _context.Invoke(d => d.Delay(interval, cancellationToken));
+      }
+    }
+}
---
+++ test/Bandwidth.Net.Test/Mocks/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Bandwidth.Net.Api;
+using LightMock;
+
+namespace Bandwidth.Net.Test.Mocks
+{
+  public class Message : IMessage
+  {
+    private readonly IInvocationContext<IMessage> _context;
+
+    public Message(IInvocationContext<IMessage> context)
+    {
+      _context = context;
+    }
+
+
+    public IEnumerable<Net.Api.Message> List(MessageQuery query = null, CancellationToken? cancellationToken = null)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ILazyInstance<Net.Api.Message>> SendAsync(MessageData data, CancellationToken? cancellationToken = null)
+    {
+      return _context.Invoke(m => m.SendAsync(data, cancellationToken));
+    }
+
+    public Task<SendMessageResult[]> SendAsync(MessageData[] data, CancellationToken? cancellationToken = null)
+    {
+      return _context.Invoke(m => m.SendAsync(data, cancellationToken));
+    }
+
+    public Task<Net.Api.Message> GetAsync(string messageId, CancellationToken? cancellationToken = null)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
