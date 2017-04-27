9b158277f64bb200757ee2ddd02475df4087bfb2
avbel <belchikovs@gmail.com>
2017-04-27 09:45:42 +0300

    Added ability to set version via environment variable.   Now we can publish new versions to nuget via creating new tag with version

--- src/Bandwidth.Net/deploy.cmd
+++ src/Bandwidth.Net/deploy.cmd
@@ -3,5 +3,6 @@ if not defined APPVEYOR_REPO_TAG_NAME (
     exit 0
 )
 rd /s /q bin
+set VERSION=%APPVEYOR_REPO_TAG_NAME%
 dotnet pack -c Release --include-symbols
 dotnet nuget push bin\Release\*.nupkg -s nuget.org -k %NUGET_API_KEY% || exit 0
