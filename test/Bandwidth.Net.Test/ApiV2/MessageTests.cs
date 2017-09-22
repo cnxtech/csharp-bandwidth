c417b67b4bfee17b81295f03aea00110a4b696af
Andrey Belchikov <belchikovs@gmail.com>
2017-09-22 10:53:19 +0300

    Fixed warnings with versions

--- test/Bandwidth.Net.Test/ApiV2/MessageTests.cs
+++ test/Bandwidth.Net.Test/ApiV2/MessageTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Linq;
+using System;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Bandwidth.Net.ApiV2;
 using LightMock;
